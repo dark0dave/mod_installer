c2d8fda38371e9fdf6331ac1f65640a0686b3d5c
dark0dave <dark0dave@mykolab.com>
2023-04-18 15:54:55 +0100

    feat: Interactive
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 # Infinity Engine Mod Installer
-      /\/\   ___   __| | (_)_ __  ___| |_ __ _| | | ___ _ __ 
+      /\/\   ___   __| | (_)_ __  ___| |_ __ _| | | ___ _ __
      /    \ / _ \ / _` | | | '_ \/ __| __/ _` | | |/ _ \ '__|
-    / /\/\ \ (_) | (_| | | | | | \__ \ || (_| | | |  __/ |   
-    \/    \/\___/ \__,_| |_|_| |_|___/\__\__,_|_|_|\___|_|   
+    / /\/\ \ (_) | (_| | | | | | \__ \ || (_| | | |  __/ |
+    \/    \/\___/ \__,_| |_|_| |_|___/\__\__,_|_|_|\___|_|
 
 ```sh
-Usage: mod_installer [OPTIONS] --log-file <LOG_FILE> --game-directory <GAME_DIRECTORY> --weidu-binary <WEIDU_BINARY> --mod-directories <MOD_DIRECTORIES>
+Usage: mod_installer [OPTIONS] --log-file <LOG_FILE> \
+  --game-directory <GAME_DIRECTORY> \
+  --weidu-binary <WEIDU_BINARY> \
+  --mod-directories <MOD_DIRECTORIES>
 
 Options:
-      --log-file <LOG_FILE>                Full path to target log [env: LOG_FILE=]
+  --log-file <LOG_FILE>                Full path to target log [env: LOG_FILE=]
   -g, --game-directory <GAME_DIRECTORY>    Full path to game directory [env: GAME_DIRECTORY=]
   -w, --weidu-binary <WEIDU_BINARY>        Full Path to weidu binary [env: WEIDU_BINARY=]
   -m, --mod-directories <MOD_DIRECTORIES>  Full Path to mod directories [env: MOD_DIRECTORIES=]
   -l, --language <LANGUAGE>                Game Language [default: en_US]
   -h, --help                               Print help
   -V, --version                            Print version
+```
+
+## Log levels
+
+Additional information can be shown with:
+```sh
+RUST_LOG=INFO mod_installer [OPTIONS]
+```
+
+For line by line debuging:
+```sh
+RUST_LOG=DEBUG mod_installer [OPTIONS]
+```
+
+To print everything including weidu logs:
+```sh
+RUST_LOG=TRACE mod_installer [OPTIONS]
 ```
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,14 @@ mod weidu;
 
 fn main() {
     env_logger::init();
-    println!(r"  /\/\   ___   __| | (_)_ __  ___| |_ __ _| | | ___ _ __ ");
-    println!(r" /    \ / _ \ / _` | | | '_ \/ __| __/ _` | | |/ _ \ '__|");
-    println!(r"/ /\/\ \ (_) | (_| | | | | | \__ \ || (_| | | |  __/ |   ");
-    println!(r"\/    \/\___/ \__,_| |_|_| |_|___/\__\__,_|_|_|\___|_|   ");
+    println!(
+        r"
+                 /\/\   ___   __| | (_)_ __  ___| |_ __ _| | | ___ _ __
+                /    \ / _ \ / _` | | | '_ \/ __| __/ _` | | |/ _ \ '__|
+               / /\/\ \ (_) | (_| | | | | | \__ \ || (_| | | |  __/ |
+               \/    \/\___/ \__,_| |_|_| |_|___/\__\__,_|_|_|\___|_|
+        "
+    );
     let args = Args::parse();
 
     create_weidu_log_if_not_exists(&args.game_directory);
@@ -31,7 +35,7 @@ fn main() {
         let mod_folder_locations = args
             .mod_directories
             .iter()
-            .flat_map(|mod_folder| find_mod_folder(&weidu_mod.name, mod_folder))
+            .flat_map(|mod_folder| find_mod_folder(&weidu_mod, mod_folder))
             .collect::<Vec<PathBuf>>();
 
         let mod_folder = if let Some(mod_folder) = mod_folder_locations.first() {
@@ -45,13 +49,10 @@ fn main() {
         if !mod_folder_present_in_game_directory(&args.game_directory, &weidu_mod.name) {
             log::info!(
                 "Copying mod directory, from {:?} to, {:?}",
-                mod_folder.join(weidu_mod.name.clone()),
+                mod_folder,
                 args.game_directory.clone().join(weidu_mod.name.clone())
             );
-            copy_mod_folder(
-                &args.game_directory,
-                &mod_folder.join(weidu_mod.name.clone()),
-            )
+            copy_mod_folder(&args.game_directory, mod_folder)
         }
         let weidu_args = generate_args(&weidu_mod, &args.language);
         install(&args.weidu_binary, &args.game_directory, weidu_args);

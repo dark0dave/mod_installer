658d8003e6b93fdc657cc7ff4878007b2cf56ddb
dark0dave <dark0dave@mykolab.com>
2024-04-14 00:25:11 +0100

    fix(parsing): Fix parsing error for windows closes #70
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/mod_component.rs
+++ src/mod_component.rs
@@ -1,7 +1,7 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    path::{PathBuf, MAIN_SEPARATOR},
+    path::PathBuf,
 };
 
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
@@ -21,36 +21,60 @@ impl From<String> for ModComponent {
 
         let install_path = parts
             .nth(1)
-            .unwrap_or_else(|| panic!("Could not get full name of mod, from: {}", line))
+            .unwrap_or_else(|| {
+                panic!(
+                    "Could not get full name of mod, from provided string: {}",
+                    line
+                )
+            })
             .to_string();
 
-        let tp_file = install_path
-            .split(MAIN_SEPARATOR)
-            .nth(1)
-            .unwrap_or_else(|| panic!("Could not find tp2 file, from: {}", line))
-            .to_string();
-
-        let name = install_path
-            .split(MAIN_SEPARATOR)
-            .next()
-            .unwrap_or_else(|| panic!("Could not split {} into mod into name and component", line))
-            .to_ascii_lowercase();
+        // This allows for both linux, macos and windows parsing
+        let (tp_file, name) = if let Some(windows_path) = install_path.split('\\').nth(1) {
+            let name = install_path
+                .split('\\')
+                .next()
+                .unwrap_or_else(|| {
+                    panic!("Could not split {} into mod into name and component", line)
+                })
+                .to_ascii_lowercase();
+            (windows_path.to_string(), name)
+        } else if let Some(linux_path) = install_path.split('/').nth(1) {
+            let name = install_path
+                .split('/')
+                .next()
+                .unwrap_or_else(|| {
+                    panic!("Could not split {} into mod into name and component", line)
+                })
+                .to_ascii_lowercase();
+            (linux_path.to_string(), name)
+        } else {
+            panic!(
+                "Could not find tp2 file name, from provided string: {}",
+                line
+            )
+        };
 
         let mut tail = parts
             .next()
-            .unwrap_or_else(|| panic!("Could not find lang and component, from {}", line))
+            .unwrap_or_else(|| {
+                panic!(
+                    "Could not find lang and component, from provided string {}",
+                    line
+                )
+            })
             .split("//");
 
         let mut lang_and_component = tail.next().unwrap_or_default().split(' ');
 
         let lang = lang_and_component
             .nth(1)
-            .unwrap_or_else(|| panic!("Could not find lang, from: {}", line))
+            .unwrap_or_else(|| panic!("Could not find lang, from provided string: {}", line))
             .replace('#', "");
 
         let component = lang_and_component
             .next()
-            .unwrap_or_else(|| panic!("Could not find component, from {}", line))
+            .unwrap_or_else(|| panic!("Could not find component, from provided string {}", line))
             .replace('#', "");
 
         let mut component_name_sub_component_version = tail.next().unwrap_or_default().split(':');
@@ -112,10 +136,10 @@ pub fn parse_weidu_log(weidu_log_path: PathBuf) -> Vec<ModComponent> {
 
 #[cfg(test)]
 mod tests {
-
+    use super::*;
+    use pretty_assertions::assert_eq;
     use std::path::Path;
 
-    use super::*;
     #[test]
     fn test_parse_weidu_log() {
         let test_log = Path::new("fixtures/test.log");
@@ -171,5 +195,21 @@ mod tests {
                 }
             ]
         );
+    }
+
+    #[test]
+    fn test_parse_windows() {
+        let mod_string = r"~TOBEX\TOBEX.TP2~ #0 #100 // TobEx - Core: v28";
+        let mod_component = ModComponent::from(mod_string.to_string());
+        let expected = ModComponent {
+            tp_file: "TOBEX.TP2".to_string(),
+            name: "tobex".to_string(),
+            lang: "0".to_string(),
+            component: "100".to_string(),
+            component_name: "TobEx - Core".to_string(),
+            sub_component: "".to_string(),
+            version: "v28".to_string(),
+        };
+        assert_eq!(mod_component, expected)
     }
 }

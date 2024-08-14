f0b1af3d8bb033c3154930c2b4c1077582e843ea
dark0dave <dark0dave@mykolab.com>
2024-08-14 15:03:54 +0100

    feat(version): Check versioning
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

---
+++ check-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -eou pipefail
+
+release_tag=$(git describe --tags --abbrev=0);
+trap "rm -f Cargo.toml.test" SIGINT;
+sed "s/^version \= .*/version = \"${release_tag/v/}\"/" Cargo.toml > Cargo.toml.test;
+diff -sd Cargo.toml.test Cargo.toml;
+if [ $? -eq 0 ]; then
+  rm -f Cargo.toml.test;
+  exit 0;
+else
+  exit 1;
+fi

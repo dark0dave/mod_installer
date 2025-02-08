88f3d774f3647a0be1a1b0204e1dc6756b16e466
dark0dave <dark0dave@mykolab.com>
2025-02-08 18:00:08 +0000

    chore(deps): Update deps
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.pre-commit
-    pkgs.rustup
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "rust-env";
+  nativeBuildInputs = [
+    # Build-time Additional Dependencies
+    cargo
+    pkg-config
+    rustc
   ];
+  buildInputs = [
+    # Run-time Additional Dependencies
+    git
+    gnupg
+    pre-commit
+    rustup
+  ];
+
+  # Set Environment Variables
+  RUST_BACKTRACE = 1;
 }

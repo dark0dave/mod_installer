35255a0183b4c777c0151307b819464fb3d622ad
dark0dave <dark0dave@mykolab.com>
2025-07-15 20:24:46 +0100

    feat(11.3.0): Prep 11.3.0
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "mod_installer";
-  version = "11.2.0";
+  version = "11.3.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
   buildInputs = with pkgs; [

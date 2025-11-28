fd5d4a64b0a727182a09587c30dc1fee00779ec4
dark0dave <dark0dave@mykolab.com>
2025-11-28 01:50:21 +0000

    fix(build): Fixes and improvements for build nix
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
+let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "mod_installer";
-  version = "12.0.1";
+  pname = manifest.name;
+  version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
   buildInputs = with pkgs; [

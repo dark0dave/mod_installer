ce126cc8308ff302394d4a45f0bb199575690a9f
Ilya Lapshin <podcherklife@gmail.com>
2025-12-02 05:45:46 +0300

    Bell char from weidu is treated as a question now

--- src/weidu_parser.rs
+++ src/weidu_parser.rs
@@ -1,4 +1,6 @@
 use std::{
+    collections::VecDeque,
+    mem,
     sync::{
         Arc,
         atomic::{AtomicUsize, Ordering},
@@ -24,50 +26,75 @@ pub(crate) fn parse_raw_output(
     timeout: usize,
 ) {
     let mut current_state = ParserState::LookingForInterestingOutput;
-    let mut question = String::new();
     sender
         .send(State::InProgress)
         .expect("Failed to send process start event");
     thread::spawn(move || {
+        let mut question = String::new();
+        let messages_to_store = 3;
+        let mut last_few_lines = VecDeque::<String>::with_capacity(messages_to_store);
         loop {
             match receiver.try_recv() {
-                Ok(string) => match current_state {
-                    ParserState::CollectingQuestion
-                    | ParserState::WaitingForMoreQuestionContent => {
-                        if parser_config.useful_status_words.contains(&string) {
-                            log::debug!(
-                                "Weidu seems to know an answer for the last question, ignoring it"
-                            );
-                            current_state = ParserState::LookingForInterestingOutput;
-                            question.clear();
-                        } else {
-                            log::debug!("Appending line '{string}' to user question");
-                            question.push_str(&string);
-                            current_state = ParserState::CollectingQuestion;
+                Ok(string) => {
+                    if last_few_lines.len() == messages_to_store {
+                        last_few_lines.pop_front();
+                    }
+                    last_few_lines.push_back(string.clone());
+
+                    match current_state {
+                        ParserState::CollectingQuestion
+                        | ParserState::WaitingForMoreQuestionContent => {
+                            if parser_config.useful_status_words.contains(&string) {
+                                log::debug!(
+                                    "Weidu seems to know an answer for the last question, ignoring it"
+                                );
+                                current_state = ParserState::LookingForInterestingOutput;
+                                question.clear();
+                            } else {
+                                log::debug!("Appending line '{string}' to user question");
+                                question.push_str(&string);
+                                current_state = ParserState::CollectingQuestion;
+                            }
+                        }
+                        ParserState::LookingForInterestingOutput => {
+                            let installer_state =
+                                parser_config.detect_weidu_finished_state(&string);
+                            if installer_state != State::InProgress {
+                                sender
+                                    .send(installer_state)
+                                    .expect("Failed to send process error event");
+                                break;
+                            }
+                            if parser_config
+                                .string_looks_like_weidu_requested_input_explicidly(&string)
+                            {
+                                let prev_lines = last_few_lines.drain(..).fold(
+                                    String::new(),
+                                    |mut acc, chunk| {
+                                        acc.push_str(&chunk);
+
+                                        acc
+                                    },
+                                );
+                                log::warn!(
+                                    "Weidu unexpectedly requested input, sending previous lines to user",
+                                );
+                                send_question_to_user(&sender, &mut current_state, prev_lines);
+                            } else if parser_config.string_looks_like_question(&string) {
+                                log::debug!(
+                                    "Changing parser state to '{:?}' due to line {}",
+                                    ParserState::CollectingQuestion,
+                                    string
+                                );
+                                current_state = ParserState::CollectingQuestion;
+                                question.push_str(string.as_str());
+                            }
+                            if !string.trim().is_empty() {
+                                log::trace!("{string}");
+                            }
                         }
                     }
-                    ParserState::LookingForInterestingOutput => {
-                        let installer_state = parser_config.detect_weidu_finished_state(&string);
-                        if installer_state != State::InProgress {
-                            sender
-                                .send(installer_state)
-                                .expect("Failed to send process error event");
-                            break;
-                        }
-                        if parser_config.string_looks_like_question(&string) {
-                            log::debug!(
-                                "Changing parser state to '{:?}' due to line {}",
-                                ParserState::CollectingQuestion,
-                                string
-                            );
-                            current_state = ParserState::CollectingQuestion;
-                            question.push_str(string.as_str());
-                        }
-                        if !string.trim().is_empty() {
-                            log::trace!("{string}");
-                        }
-                    }
-                },
+                }
                 Err(TryRecvError::Empty) => match current_state {
                     ParserState::CollectingQuestion => {
                         log::debug!(
@@ -78,11 +105,11 @@ pub(crate) fn parse_raw_output(
                     }
                     ParserState::WaitingForMoreQuestionContent => {
                         log::debug!("No new weidu output, sending question to user");
-                        sender
-                            .send(State::RequiresInput { question })
-                            .expect("Failed to send question");
-                        current_state = ParserState::LookingForInterestingOutput;
-                        question = String::new();
+                        send_question_to_user(
+                            &sender,
+                            &mut current_state,
+                            mem::take(&mut question),
+                        );
                     }
                     _ if wait_count.load(Ordering::Relaxed) >= timeout => {
                         sender
@@ -100,4 +127,15 @@ pub(crate) fn parse_raw_output(
             }
         }
     });
+}
+
+fn send_question_to_user(
+    sender: &Sender<State>,
+    current_state: &mut ParserState,
+    question: String,
+) {
+    sender
+        .send(State::RequiresInput { question: question })
+        .expect("Failed to send question");
+    *current_state = ParserState::LookingForInterestingOutput;
 }

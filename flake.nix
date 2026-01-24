bf4c76b4be2de9d7dab72c420d02a1cee8b8826f
dark0dave <dark0dave@mykolab.com>
2026-01-24 19:27:06 +0900

    build(gui): Add gui to release
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- flake.nix
+++ flake.nix
@@ -14,24 +14,35 @@
           pkgs = import nixpkgs { inherit system; };
           overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
         in {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              cargo
-              clippy
-              git
-              pkg-config
-              pre-commit
-              rust-analyzer
-              rustc
-              rustfmt
-            ];
-            buildInputs = with pkgs; [
-              openssl
-            ];
-            env.RUSTC_VERSION = overrides.toolchain.channel;
- #           env.CARGO_HOME = "";
- #           env.RUSTUP_HOME = "";
-          };
+          default =
+            with pkgs;
+            mkShell rec {
+                nativeBuildInputs = [
+                    cargo
+                    clippy
+                    git
+                    pkg-config
+                    pre-commit
+                    rust-analyzer
+                    rustc
+                    rustfmt
+                    xorg.libX11
+                    xorg.libXcursor
+                    xorg.libXrandr
+                    xorg.libXi
+                    xorg.libxcb
+                    libxkbcommon
+                    vulkan-loader
+                    wayland
+                ];
+                buildInputs = [
+                    openssl
+                ];
+                env.RUSTC_VERSION = overrides.toolchain.channel;
+                shellHook = ''
+                    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath nativeBuildInputs)}";
+                '';
+            };
         });
       packages = forEachSystem (system: {
         default = pkgsFor.${system}.callPackage ./default.nix { };
